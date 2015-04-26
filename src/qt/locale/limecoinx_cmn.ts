63792aa204daed28c3afdacd6e2e8dc5f0be79b6
Christian Knöpke <apfelbaum@email.de>
2015-04-26 18:07:48 +0200

    Update limecoinx_cmn.ts

--- src/qt/locale/limecoinx_cmn.ts
+++ src/qt/locale/limecoinx_cmn.ts
@@ -13,7 +13,7 @@
     </message>
     <message>
         <location filename="../forms/aboutdialog.ui" line="94"/>
-        <source>Copyright &amp;copy; 2009-2014 The Bitcoin Core developers.
+<source>Copyright &amp;copy; 2009-2014 The Bitcoin/DASH Core developers.
 Copyright &amp;copy; 2014-YYYY The Limx Core developers.</source>
         <translation>版权 &amp;copy; 2009-2014 比特币核心的开发者。
 版权 &amp;copy; 达世币核心的开发者。</translation>
