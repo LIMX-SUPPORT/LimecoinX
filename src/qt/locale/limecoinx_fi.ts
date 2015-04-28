8d2907713589d83aa2dc2311b3ed59a36288953f
Christian Knöpke <apfelbaum@email.de>
2015-04-28 10:47:45 +0200

    Update limecoinx_fi.ts

--- src/qt/locale/limecoinx_fi.ts
+++ src/qt/locale/limecoinx_fi.ts
@@ -15,8 +15,7 @@
         <location filename="../forms/aboutdialog.ui" line="94"/>
         <source>Copyright &amp;copy; 2009-YYYY The Bitcoin and DASH Core developers.
 Copyright &amp;copy; 2014-YYYY The Limx Core developers.</source>
-        <translation>Tekijänoikeus &amp;copy; 2009-2014 Bitcoin Core kehittäjät.
-Tekijänoikeus &amp;copy; 2014-YYYY Limx Core kehittäjät.</translation>
+ <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../forms/aboutdialog.ui" line="111"/>
