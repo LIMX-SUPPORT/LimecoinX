a33e47e49012caf186681ce71621c5e011794c5c
Christian Knöpke <apfelbaum@email.de>
2015-05-01 16:40:53 +0200

    Update utilitydialog.cpp

--- src/qt/utilitydialog.cpp
+++ src/qt/utilitydialog.cpp
@@ -57,7 +57,8 @@ AboutDialog::AboutDialog(QWidget *parent) :
 
     // Set current copyright year
     ui->copyrightLabel->setText(tr("Copyright") + QString(" &copy; 2009-%1 ").arg(COPYRIGHT_YEAR) + tr("The Bitcoin and Dash Core developers") + QString("<br />")
-                              + tr("Copyright") + QString(" &copy; 2014-%1 ").arg(COPYRIGHT_YEAR) + tr("The Limecoinx Core developers"));
+                                 + tr("Copyright") + QString(" &copy; 2014  ")+ tr("Paperwallet by Dodge Core developers") + QString("<br />")
+                                 + tr("Copyright") + QString(" &copy; 2014-%1 ").arg(COPYRIGHT_YEAR) + tr("The LIMX Core developers."));
 }
 
 void AboutDialog::setModel(ClientModel *model)
