d1fc05c037baf3803eabdb3d81251ff108501515
Christian Knöpke <apfelbaum@email.de>
2015-05-23 09:27:35 +0200

    Update sendcoinsdialog.cpp

--- src/qt/sendcoinsdialog.cpp
+++ src/qt/sendcoinsdialog.cpp
@@ -58,6 +58,8 @@ SendCoinsDialog::SendCoinsDialog(QWidget *parent) :
     }
     connect(ui->checkUseDarksend, SIGNAL(stateChanged ( int )), this, SLOT(updateDisplayUnit()));
     connect(ui->checkInstantX, SIGNAL(stateChanged ( int )), this, SLOT(updateInstantX()));
+    // Just test Limxdev 23-05-2015
+    CoinControlDialog::coinControl->useDarkSend = false;
 
     // Coin Control: clipboard actions
     QAction *clipboardQuantityAction = new QAction(tr("Copy quantity"), this);
