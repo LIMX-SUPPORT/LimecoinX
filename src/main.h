e45d0bbc3f9062d8aabfd1b0a4ceedb9a4153461
Christian Knöpke <apfelbaum@email.de>
2015-06-05 22:44:47 +0200

    Update main.h

--- src/main.h
+++ src/main.h
@@ -35,9 +35,9 @@ enum DiffMode {
     DIFF_DEFAULT = 0, // Default to invalid 0
     DIFF_BTC     = 1, // Retarget every x blocks (Bitcoin style)
     DIFF_KGW     = 2,
-    DIFF_KGW2    = 2,// Retarget using Kimoto Gravity Well
-    DIFF_DGW     = 3,
-    DIFF_DELTA   = 3,
+    DIFF_KGW2    = 3,// Retarget using Kimoto Gravity Well
+    DIFF_DGW     = 4,
+    DIFF_DELTA   = 5,
     DIFF_NULL    = 0,// Retarget using Dark Gravity Wave v3
 };
 
