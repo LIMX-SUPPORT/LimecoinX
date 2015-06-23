b2255e1cd5154f8ef15a14c02006a24962d16611
Christian Knöpke <apfelbaum@email.de>
2015-06-23 20:38:01 +0200

    Just a test

--- src/miner.cpp
+++ src/miner.cpp
@@ -512,8 +512,8 @@ bool CheckWork(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey)
 
         // Process this block the same as if we had received it from another node
         CValidationState state;
-        if (!ProcessBlock(state, NULL, pblock))
-            return error("DarkcoinMiner : ProcessBlock, block not accepted");
+       // if (!ProcessBlock(state, NULL, pblock))
+       //     return error("DarkcoinMiner : ProcessBlock, block not accepted");
     }
 
     return true;
